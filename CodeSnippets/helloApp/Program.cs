fad66ee8a9805854dbfeea1601048b6aab248a2d
ankurats <ankur.ats@hotmail.com>
2018-12-10 20:00:37 +0530

    Add files via upload

---
+++ CodeSnippets/helloApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+namespace helloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+			.UseKestrel()
+			.UseStartup<Startup>()
+			.Build();
+
+		host.Run();
+        }
+    }
+}
---
+++ CodeSnippets/helloApp/Startup.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+
+namespace helloApp{
+public class Startup{
+
+	public void Configure(IApplicationBuilder app)
+	{
+	app.Run(context =>{
+		return context.Response.WriteAsync("Hello from Aspnet core");
+	});
+	}	
+}
+}
